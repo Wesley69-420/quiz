16ef143b0c9a55228b54b85f48fd38e0d1b3a48d
Wesley69-420 <76279861+Wesley69-420@users.noreply.github.com>
2021-10-10 14:53:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvas,backgroundImg;
+var gameState = 0;
+var answer,database;
+var question,contestant,quiz;
+var contestantCount,allContestants
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+
+  }
+  
+  if (gameState === 1){
+    clear();
+    quiz.play();
+  }
+
+  
+}
